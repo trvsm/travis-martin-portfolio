05d903576160ee5ec86fd19bbad130f7770feb4f
travis <trvsmartin@gmail.com>
2022-12-17 15:13:55 -0800

    add hero image & placeholder
    <body>
    
    Issue #

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from "react";
 
 import "./App.scss";
 import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
+  return (<>
       <Header />
+      <Hero />
 
+    <div className="App">
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
     </div>
+  </>
   );
 }
 
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import "./hero.scss";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="hero__title">
+        "He lingered there, watching an eddy that turned and turned, until the
+        stream absorbed it, and carried it on to the sea. -'Like me!'" -Dickens
+      </h1>
+    </div>
+  );
+}
