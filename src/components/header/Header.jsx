b12303c90e7afad713294808d08b184dd07e1239
travis <trvsmartin@gmail.com>
2022-12-28 12:06:26 -0800

    style mobile hero & projects
    <body>
    
    Issue #

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,13 @@ import "./header.scss";
 export default function Header() {
   return (
     <div className="nav">
-      <section class="nav">
+      <section className="nav">
         <div className="nav__logo">Travis Martin</div>
         <input id="menu-toggle" type="checkbox" />
-        <label class="menu-button-container" for="menu-toggle">
-          <div class="menu-button"></div>
+        <label className="menu-button-container" htmlFor="menu-toggle">
+          <div className="menu-button"></div>
         </label>
-        <ul class="menu">
+        <ul className="menu">
           <li className="nav__link">Home</li>
           <li className="nav__link">About</li>
           <li className="nav__link">Projects</li>
@@ -17,7 +17,7 @@ export default function Header() {
         </ul>
       </section>
       <a className="nav__link" href="#">
-        <div className="nav__logo">Travis Martin</div>
+        <div className="nav__logo"></div>
       </a>
       <ul className="nav__links">
         <li className="nav__link">Home</li>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,12 +2,19 @@ import "./hero.scss";
 
 export default function Hero() {
   return (
-    <div className="hero">
+    <section className="hero">
+      <div className="hero__top">
+
       <h1 className="hero__title">
         "He lingered there, watching an eddy that turned and turned, until the
         stream absorbed it, and carried it on to the sea. -'Like me!'" -Dickens
       </h1>
-      <p className="hero__text">personal tagline/hook/about</p>
-    </div>
+      <p className="hero__text">
+        Web Developer | JavaScript | React | Express
+        <br/>Thinking about how tech can serve global good
+      </p>
+      </div>
+      <p className="hero__photo">"Kekabeka Falls 2020" by Travis Martin</p>
+    </section>
   );
 }
