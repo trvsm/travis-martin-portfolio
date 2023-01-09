f4a7cea65fde5cbe6e26cc98c78793bc8ada7329
travis <trvsmartin@gmail.com>
2023-01-09 10:06:22 -0800

    add About copy
    <body>
    
    Issue #

--- src/components/elevatorBio/ElevatorBio.jsx
+++ src/components/elevatorBio/ElevatorBio.jsx
@@ -3,9 +3,10 @@ import "./elevatorBio.scss";
 export default function ElevatorBio() {
   return (
     <div id="about" className="elevator__container">
-      <h2 className="elevator__title">Inventory coordinator turned programmer</h2>
+      <h2 className="elevator__title">Daily refining my grasp of Web Development</h2>
       <hr />
-      2-3 sentence personal paragraph
+      <p className="elevator__context">A person is a product of their environment, with agency to decide how they respond to it.</p>
+      <p className="elevator__why">After a cross-Canada move &#40;closing the brick & mortar game shop I founded&#41; I found myself searching for philosophical direction as well as vocational direction.  I decided to pursue Web Development with the understanding technology can do widespread good.  Computing power creates opportunities to scale individual impact.  Programming appeals to me as the pursuit of technical details and how they fit together</p>
     </div>
   );
 }
