d5db5f52161afd557cf5e21f35df8c4de3cfc800
travis <trvsmartin@gmail.com>
2023-01-09 15:13:39 -0800

    add project images & descriptions
    <body>
    
    Issue #

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,12 +5,35 @@ export default function Projects() {
     <div className="projects" id="projects">
       <h2 className="projects__title">My projects</h2>
       <div className="projects__gallery">
-        <div className="projects__feature">Meal Planner</div>
-        <div className="projects__feature">
+        <div className="projects__feature projects__feature--meal">
+          <h3 className="projects__name">Meal Planner</h3>
+          <p className="projects__description">
+            View & select recipes, compile a shopping list. React, Express, Node
+          </p>
+        </div>
+        <a
+          href="https://github.com/trvsm/ea-industry-frontend"
+          target={"blank"}
+          className="projects__feature projects__feature--ea"
+        >
           <h3 className="projects__name">
             BrainStation/EA Industry Partner Hackathon
-          </h3></div>
-        <div className="projects__feature">Coding Exercises</div>
+          </h3>
+          <p className="projects__description">
+            24 Hour hackathon. Mission: encourage adoption of the EA App. React
+            SPA
+          </p>
+        </a>
+        <a
+          href="https://github.com/trvsm/coding-exercises"
+          target={"blank"}
+          className="projects__feature projects__feature--exercise"
+        >
+          <h3 className="projects__name">Coding Exercises</h3>
+          <p className="projects__description">
+            Daily notes & Exercises, JavaScript
+          </p>
+        </a>
       </div>
     </div>
   );
